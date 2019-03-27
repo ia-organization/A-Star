864a89737086787a2be48f8c7faa2d1545ba0b32
Jonathan Ribeiro da Silva <jota.ribeiro.silva@gmail.com>
2019-03-26 23:15:41 -0300

    alterações no aStar

--- astar.py
+++ astar.py
@@ -9,26 +9,27 @@ class Node():
     def move_cost(self,other):
         return 0 if self.value == '.' else 1
 
-    def __eq__(self, other):
-        return self.position == other.position
-
         
 def children(point,grid):
     x,y = point.point
-    links = [grid[d[0]][d[1]] for d in [(x-1, y),(x,y - 1),(x,y + 1),(x+1,y)]]
+    print(len(grid))
+    print(len(grid[0]))
+    print ([(0 if (x-1) < 0 else x-1, y),(x,0 if (y - 1)< 0 else y-1),(x,y + 1),(x+1,y)])
+    links = [grid[d[0]][d[1]] for d in [(0 if (x-1) < 0 else x-1, y),(x,0 if (y - 1)< 0 else y-1),(x,y + 1),(x+1,y)]]
     return [link for link in links if link.value != '%']
+
 def manhattan(point,point2):
     return abs(point.point[0] - point2.point[0]) + abs(point.point[1]-point2.point[0])
 
 def aStar(start, goal, grid):
-    openset = set()
-    closedset = set()
+    openset = []
+    closedset = []
 
-    #Ponto de partida é o ponto inicial
+    #Ponto de partida   é o ponto inicial
     current = start
 
     #Adicionando o ponto inicial a lista aberta
-    openset.add(current)
+    openset.append(current)
 
     #Enquanto houver elementos na lista aberta
     while openset:
@@ -45,7 +46,7 @@ def aStar(start, goal, grid):
         #Remove the item from the open set
         openset.remove(current)
         #Add it to the closed set
-        closedset.add(current)
+        closedset.append(current)
         #Loop through the node's children/siblings
         for node in children(current,grid):
             #If it is already in the closed set, skip it
@@ -66,31 +67,35 @@ def aStar(start, goal, grid):
                 #Set the parent to our current item
                 node.parent = current
                 #Add it to the set
-                openset.add(node)
+                openset.append(node)
     #Throw an exception if there is no path
     raise ValueError('No Path Found')
 def next_move(pacman,food,grid):
     #Convert all the points to instances of Node
-    for x in xrange(len(grid)):
-        for y in xrange(len(grid[x])):
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
             grid[x][y] = Node(grid[x][y],(x,y))
     #Get the path
+    print(len(grid))
+    print(len(grid[0]))
+    print(pacman)
+    print(food)
     path = aStar(grid[pacman[0]][pacman[1]],grid[food[0]][food[1]],grid)
     #Output the path
-    print len(path) - 1
+    print (len(path) - 1)
     for node in path:
         x, y = node.point
-        print x, y
-pacman_x, pacman_y = [ int(i) for i in raw_input().strip().split() ]
-food_x, food_y = [ int(i) for i in raw_input().strip().split() ]
-x,y = [ int(i) for i in raw_input().strip().split() ]
+        print (x, y)
+pacman_x = int(input())
+pacman_y = int(input())
+food_x = int(input())
+food_y = int(input())
+x = int(input())
+y = int(input())
+
  
 grid = []
-for i in xrange(0, x):
-    grid.append(list(raw_input().strip()))
+for i in range(0, x):
+    grid.append(input().split())
  
 next_move((pacman_x, pacman_y),(food_x, food_y), grid)
-
-
-if __name__ == '__main__':
-    main()
